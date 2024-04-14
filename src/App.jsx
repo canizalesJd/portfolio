7e378826f08881499f51afe57d5f9ced12a52d50
Jose Canizales <canizalesjd@gmail.com>
2024-04-14 11:59:17 -0600

    Adding work experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
+import Experience from "./components/Experience";
 
 export default function App() {
 	return (
 		<main className="dark text-foreground bg-background min-h-screen min-w-scren overflow-hidden">
 			<Navigation />
 			<Hero />
+			<Experience />
 		</main>
 	);
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,9 @@ import { MagneticText } from "./ui/MagneticText";
 import LinkedInIcon from "./icons/LinkedInIcon";
 const Hero = () => {
 	return (
-		<div className="hero container  mx-auto h-[30rem] max-h-scren flex flex-col justify-center items-center">
+		<div className="hero container  mx-auto h-[30rem] max-h-scren flex flex-col justify-center max-w-2xl">
 			<div className="h-[35rem] absolute inset-0 bg-grid-white/10 bg-grid-16 [mask-image:linear-gradient(180deg,black,transparent)]"></div>
-			<div className="mx-auto pl-5">
+			<div className="pl-5">
 				<Image
 					width={150}
 					height={150}
@@ -29,7 +29,7 @@ const Hero = () => {
 						))
 					}
 				</MagneticText>
-				<p className="text-lg max-w-xl font-light mt-1">
+				<p className="text-lg max-w-xl font-light mt-1 text-gray-400">
 					2+ years of experience. Based in San Jose, Costa Rica.
 				</p>
 				<p className="text-xl max-w-xl mt-1">

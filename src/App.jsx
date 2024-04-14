fdd2deb3e326270c2eda9094a61e682480ba3100
Jose Canizales <canizalesjd@gmail.com>
2024-04-14 06:52:36 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Navigation from "./components/Navigation";
+
+export default function App() {
+	return (
+		<main className="text-foreground bg-background min-h-screen min-w-scren overflow-hidden">
+			<Navigation />
+		</main>
+	);
+}
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,38 @@
+import {
+	Navbar,
+	NavbarBrand,
+	NavbarContent,
+	NavbarItem,
+	Link,
+} from "@nextui-org/react";
+
+const Navigation = () => {
+	return (
+		<Navbar isBordered shouldHideOnScroll>
+			<NavbarBrand>
+				<h1 className="text-xl font-serif font-bold">Jose Canizales</h1>
+			</NavbarBrand>
+			<NavbarContent className="hidden sm:flex gap-4" justify="center">
+				<NavbarItem>
+					<Link color="foreground" href="#">
+						Features
+					</Link>
+				</NavbarItem>
+				<NavbarItem isActive>
+					<Link href="#" aria-current="page">
+						Customers
+					</Link>
+				</NavbarItem>
+				<NavbarItem>
+					<Link color="foreground" href="#">
+						Integrations
+					</Link>
+				</NavbarItem>
+			</NavbarContent>
+			<NavbarContent justify="end">
+				<NavbarItem>Dark / Light</NavbarItem>
+			</NavbarContent>
+		</Navbar>
+	);
+};
+export default Navigation;
